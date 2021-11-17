7ba839005b7230f162a6b03a8dc16b3b123a2bc1
z3tsubouou <davgatseren@gmail.com>
2021-11-17 11:51:34 +0800

    exm

--- Scripts/input.js
+++ Scripts/input.js
@@ -4,17 +4,24 @@ const defaultBtn = document.querySelector("#default-btn")
 const customBtn = document.querySelector("#custom-btn")
 const cancelBtn = document.querySelector("#cancel-btn")
 const img = document.querySelector(".image img")
+const uploadedImage = document.querySelector("#uploaded_image")
+const uploadImageForm = document.querySelector("#uploadImageForm")
 let regExp =  /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
 function defaultBtnActive(){
     defaultBtn.click();
 }
+
+var imageDataBase64 = ""
+
 defaultBtn.addEventListener("change", function(){
     const file = this.files[0];
     if(file){
         const reader = new FileReader();
         reader.onload = function(){
             const result = reader.result;
+            imageDataBase64 = result;
             img.src = result;
+            uploadedImage.src = result;
             wrapper.classList.add("active");
         }
         cancelBtn.addEventListener("click", function(){
@@ -29,3 +36,19 @@ defaultBtn.addEventListener("change", function(){
         fileName.textContent = valueStore;
     }
 });
+
+uploadImageForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    const response = await fetch(url, {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: {
+            image: imageDataBase64.split(',')[1],
+            name: e.target.elements.name.value
+        }
+    });
+    console.log(response)
+})
